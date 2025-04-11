f840c7d5288726533ed2aa0cd5b35b6664275ece
Thanatos <nikazavgorodniaya@gmail.com>
2025-04-11 13:51:25 +0300

    update

---
+++ src/test/java/com/tanatousi/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanatousi.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
