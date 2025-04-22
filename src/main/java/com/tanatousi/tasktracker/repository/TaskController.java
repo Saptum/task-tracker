0e8e2d11e339400e945fc784388d46645de320d3
Thanatos <nikazavgorodniaya@gmail.com>
2025-04-22 22:49:51 +0300

    add new classes

---
+++ src/main/java/com/tanatousi/tasktracker/repository/TaskController.java
@@ -0,0 +1,11 @@
+package com.tanatousi.tasktracker.repository;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/tasks")
+public interface TaskController {
+}
---
+++ src/main/java/com/tanatousi/tasktracker/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.tanatousi.tasktracker.repository;
+
+import com.tanatousi.tasktracker.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
